23c52eaefcc541ed4366349c69380b56783c0248
Russell Manning <russell@digitaltempo.nz>
2025-09-12 11:57:44 +1200

    updated to url

--- src/functions/httpTrigger1.js
+++ src/functions/httpTrigger1.js
@@ -6,7 +6,7 @@ app.http('httpTrigger1', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
-        const name = request.query.get('name') || await request.text() || 'world';
+        const name = request.query.get('name') || await request.text() || 'Russell';
 
         return { body: `Hello, ${name}!` };
     }
